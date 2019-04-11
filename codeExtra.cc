990f4359ba63ea157d21926f6a88a0d954b0f80f
hayk99 <haykk1999@gmail.com>
2019-04-11 17:10:59 +0200

    añado code Extra

--- codeExtra.cc
+++ codeExtra.cc
@@ -1,8 +1,54 @@
 //Esto al .h
 
 class Ruta {
-	string rutaActual
+	string ruta;
+	list<Directorio*> rutaActual;
+	std::shared_ptr <Directorio> dirActual;
 
 public:
-	Ruta()
+	Ruta();
+	~Ruta();
+	string pwd();
+	void ls();
+	void cd(string path);
+	void stat(string element);
+};
+
+//Esto al cc
+
+Ruta::Ruta(const Directorio& root_)rutaActual("/"), dirActual(dirActual) {};
+
+Ruta::~Ruta() {};
+
+string Ruta::pwd() { 
+	return rutaActual;
+}
+
+void Ruta::ls() {
+	Directorio::mostrarELementos(p);
+}
+
+void cd(string path) {
+	dir=strtok(path, "/");
+	cout << "  " <<dir;
+	while( ( (dir=strtok(NULL, "/")) != NULL) && existe) {
+		cout << "  " << dir;
+		bool existe = existe(dir);
+	}
+	if (!existe) cout << path << "No such file or directory \n";
+}
+
+void stat(string element) {
+	string elemento;
+	if ( elemtn[0]='/') {
+		elemento=strtok(path, "/");
+		cout << "  " <<elemento;
+		while( ( (elemento=strtok(NULL, "/")) != NULL) && existe) {
+			cout << "  " << elemento;
+			bool existe = Directorio::existe(elemento);
+		}
+	}
+	else elemento=element;
+	elemento.obtenerTamanyo()
+
 }
